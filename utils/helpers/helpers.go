9152c8d7216223eaf4d9bff0cb5236c663396615
i9 <oluwarinolasam@gmail.com>
2024-03-06 23:15:26 +0100

    added pgx query functions reuse | filter lines starting with "#" in .env scanning

--- utils/helpers/helpers.go
+++ utils/helpers/helpers.go
@@ -2,9 +2,13 @@ package helpers
 
 import (
 	"bufio"
+	"context"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func LoadEnv() error {
@@ -17,7 +21,7 @@ func LoadEnv() error {
 
 	for env.Scan() {
 		key, value, found := strings.Cut(env.Text(), "=")
-		if !found {
+		if !found || strings.HasPrefix(key, "#") {
 			continue
 		}
 
@@ -28,4 +32,41 @@ func LoadEnv() error {
 	}
 
 	return nil
+}
+
+func GetDBPool() (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), os.Getenv("PGDATABASE_URL"))
+	return pool, err
+}
+
+func QueryRow[T any](sql string, params ...any) (*T, error) {
+	pool, err := GetDBPool()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, _ := pool.Query(context.Background(), sql, params...)
+
+	data, err := pgx.CollectOneRow(rows, pgx.RowToAddrOfStructByName[T])
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func QueryRows[T any](sql string, params ...any) ([]*T, error) {
+	pool, err := GetDBPool()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, _ := pool.Query(context.Background(), sql, params...)
+
+	data, err := pgx.CollectRows(rows, pgx.RowToAddrOfStructByName[T])
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
