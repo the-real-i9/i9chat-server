c75dd051c3784bb16caeb1baa7bbcbd03b6f337c
i9 <oluwarinolasam@gmail.com>
2024-03-20 14:45:06 +0100

    function extraction

--- services/chatService/groupChatService.go
+++ services/chatService/groupChatService.go
@@ -9,6 +9,30 @@ import (
 	"utils/helpers"
 )
 
+func broadcastNewGroup(initUsers [][]string, newGroupData map[string]any) {
+	for _, newMember := range initUsers[1:] { // the first user is the creator, hence, they're excluded
+		newMemberId := newMember[0]
+		go appglobals.ChatObserver{}.Send(fmt.Sprintf("user-%s", newMemberId), newGroupData, "new")
+	}
+}
+
+func uploadGroupPicture(groupChatId int, picture []byte) {
+	if len(picture) < 1 {
+		return
+	}
+	// upload picture data and SET picture url
+	picPath := fmt.Sprintf("chat_pictures/group_chat_%d_pic.jpg", groupChatId)
+
+	picUrl, err := helpers.UploadFile(picPath, picture)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	helpers.QueryRowField[bool]("UPDATE group_chat SET picture = $1 WHERE id = $2 RETURNING true", picUrl, groupChatId)
+}
+
 func NewGroupChat(name string, description string, picture []byte, creator []string, initUsers [][]string) (map[string]any, error) {
 	data, err := chatmodel.NewGroupChat(name, description, "", creator, initUsers)
 	if err != nil {
@@ -22,37 +46,23 @@ func NewGroupChat(name string, description string, picture []byte, creator []str
 
 	helpers.MapToStruct(data, &respData)
 
-	go func() {
-		for _, newMember := range initUsers[1:] { // the first user is the creator, hence, they're excluded
-			newMemberId := newMember[0]
-			go appglobals.ChatObserver{}.Send(fmt.Sprintf("user-%s", newMemberId), respData.Nmrd, "new")
-		}
-	}()
+	go uploadGroupPicture(respData.Crd["new_group_chat_id"].(int), picture)
 
-	go func() {
-		if len(picture) < 1 {
-			return
-		}
-		// upload picture and get back url
-		groupChatId := respData.Crd["new_group_chat_id"].(int)
-
-		picPath := fmt.Sprintf("chat_pictures/group_chat_%d_pic.jpg", groupChatId)
-
-		picUrl, err := helpers.UploadFile(picPath, picture)
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		helpers.QueryRowField[bool]("UPDATE group_chat SET picture = $1 WHERE id = $2 RETURNING true", picUrl, groupChatId)
-	}()
+	go broadcastNewGroup(initUsers, respData.Nmrd)
 
 	return respData.Crd, nil
 }
 
 type GroupChat struct {
 	Id int
+}
+
+func (gpc GroupChat) broadcastMessage(memberIds []int, msgData map[string]any) {
+	for _, mId := range memberIds {
+		memberId := mId
+		go appglobals.GroupChatMessageObserver{}.Send(fmt.Sprintf("user-%d--groupchat-%d", memberId, gpc.Id), msgData, "new")
+		// go appglobals.ChatObserver{}.Send(fmt.Sprintf("user-%d", memberId), map[string]any{"groupChatId": gpc.Id, "event": "new message", "senderId": senderId}, "update")
+	}
 }
 
 func (gpc GroupChat) SendMessage(senderId int, msgContent map[string]any, createdAt time.Time) (map[string]any, error) {
@@ -69,13 +79,7 @@ func (gpc GroupChat) SendMessage(senderId int, msgContent map[string]any, create
 
 	helpers.MapToStruct(data, &respData)
 
-	go func() {
-		for _, mId := range respData.MemberIds {
-			memberId := mId
-			go appglobals.GroupChatMessageObserver{}.Send(fmt.Sprintf("user-%d--groupchat-%d", memberId, gpc.Id), respData.Mrd, "new")
-			go appglobals.ChatObserver{}.Send(fmt.Sprintf("user-%d", memberId), map[string]any{"groupChatId": gpc.Id, "event": "new message"}, "update")
-		}
-	}()
+	go gpc.broadcastMessage(respData.MemberIds, respData.Mrd)
 
 	return respData.Srd, nil
 }
@@ -84,15 +88,16 @@ func (gpc GroupChat) GetChatHistory(offset int) ([]*map[string]any, error) {
 	return chatmodel.GroupChat{Id: gpc.Id}.GetChatHistory(offset)
 }
 
-type GroupMessage struct {
-	GroupChatId int
+type GroupChatMessage struct {
 	Id          int
+	GroupChatId int
+	SenderId    int
 }
 
-func (gpcm GroupMessage) React() {
+func (gpcm GroupChatMessage) UpdateDeliveryStatus() {
 
 }
 
-func (gpcm GroupMessage) UpdateDeliveryStatus() {
+func (gpcm GroupChatMessage) React() {
 
 }
