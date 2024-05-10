8ab4d1d01ad45c7153624c1bf8b161ce875038c7
i9 <oluwarinolasam@gmail.com>
2024-05-10 15:45:14 +0100

    test data

--- tests/routes/authRoutes_test/authRoutes_test.go
+++ tests/routes/authRoutes_test/authRoutes_test.go
@@ -1,9 +1,11 @@
 package authRoutes_test
 
 import (
-	"fmt"
+	"encoding/json"
+	"i9chat/tests/testdata"
 	"i9chat/utils/appTypes"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/fasthttp/websocket"
@@ -11,7 +13,11 @@ import (
 
 const pathPrefix string = "ws://localhost:8000/api/auth"
 
-var signupSessesionJwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoib2x1d2FyaW5vbGFzYW1AZ21haWwuY29tIiwic2Vzc2lvbklkIjoiMWQ5YzA0MjktN2M0ZS00NDIwLTgzZDEtNDg4Njc5OGFkMzExIn0sImp3dENsYWltcyI6eyJleHAiOiIyMDI0LTA1LTEwVDE1OjAxOjE4LjE1NjE0ODM4N1oiLCJpYXQiOiIyMDI0LTA1LTEwVDE0OjAxOjE4LjE4NjIzOTk4N1oiLCJpc3N1ZXIiOiJpOWNoYXQifX0.+Z1WSQmTlqKV+khtVwTbILho89eYOymxPHjEGXI6BJE="
+func printJSON(data map[string]any) {
+	res, _ := json.Marshal(data)
+
+	os.Stdout.Write(res)
+}
 
 func XTestRequestNewAccount(t *testing.T) {
 	wsd := &websocket.Dialer{}
@@ -47,13 +53,13 @@ func XTestRequestNewAccount(t *testing.T) {
 		return
 	}
 
-	fmt.Println(recvData.Body)
+	printJSON(recvData.Body)
 }
 
-func TestVerifyEmail(t *testing.T) {
+func XTestVerifyEmail(t *testing.T) {
 	wsd := &websocket.Dialer{}
 	reqH := http.Header{}
-	reqH.Set("Authorization", signupSessesionJwt)
+	reqH.Set("Authorization", testdata.SignupSessionJwt)
 
 	connStream, _, err := wsd.Dial(pathPrefix+"/signup/verify_email", reqH)
 	if err != nil {
@@ -86,5 +92,46 @@ func TestVerifyEmail(t *testing.T) {
 		return
 	}
 
-	fmt.Println(recvData.Body)
+	printJSON(recvData.Body)
+}
+
+func TestRegisterUser(t *testing.T) {
+	wsd := &websocket.Dialer{}
+	reqH := http.Header{}
+	reqH.Set("Authorization", testdata.SignupSessionJwt)
+
+	connStream, _, err := wsd.Dial(pathPrefix+"/signup/register_user", reqH)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer connStream.Close()
+
+	sendData := map[string]string{
+		"username":    "i9x",
+		"password":    "fhunmytor",
+		"geolocation": "5, 2, 2",
+	}
+
+	w_err := connStream.WriteJSON(sendData)
+	if w_err != nil {
+		t.Error(w_err)
+		return
+	}
+
+	var recvData appTypes.WSResp
+
+	r_err := connStream.ReadJSON(&recvData)
+	if r_err != nil {
+		t.Error(r_err)
+		return
+	}
+
+	if recvData.Error != "" {
+		t.Error(recvData.Error)
+		return
+	}
+
+	printJSON(recvData.Body)
 }
