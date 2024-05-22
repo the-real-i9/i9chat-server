f304cd03fcdc5560cc9a4b7e8dc4447332e9f6bc
i9 <oluwarinolasam@gmail.com>
2024-05-22 18:42:30 +0100

    renamed authHelpers_test to helpers_test

--- utils/helpers/helpers_test.go
+++ utils/helpers/helpers_test.go
@@ -9,7 +9,7 @@ import (
 func TestJwt(t *testing.T) {
 	token := JwtSign(map[string]any{"a": 5}, "some secret", time.Now().Add(1*time.Hour))
 
-	data, err := JwtVerify(token, "some secet")
+	data, err := JwtVerify(token, "some secret")
 
 	if err != nil {
 		t.Errorf("%s", err)
