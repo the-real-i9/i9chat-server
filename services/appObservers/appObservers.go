66652a1825aaf91ef8317c0b542e47eec9664b1a
i9 <oluwarinolasam@gmail.com>
2024-07-23 05:57:00 +0100

    observers synchronization with mutex locks

--- services/appObservers/appObservers.go
+++ services/appObservers/appObservers.go
@@ -3,6 +3,7 @@ package appObservers
 import (
 	"fmt"
 	"i9chat/utils/helpers"
+	"sync"
 )
 
 type Observer interface {
@@ -17,20 +18,32 @@ type Observer interface {
 // DMChatActiveSessionObserver - Events: ("message update"). dmChatId: (n)
 // GroupChatSessionObserver - Events: ("message update"). groupChatId: (n)
 
-var dmChatObserver = make(map[string]chan<- map[string]any)
+var (
+	muDM           = sync.Mutex{}
+	dmChatObserver = make(map[string]chan<- map[string]any)
+)
 
 type DMChatObserver struct{}
 
 func (DMChatObserver) Subscribe(key string, mailbox chan<- map[string]any) {
+	muDM.Lock()
+	defer muDM.Unlock()
+
 	dmChatObserver[key] = mailbox
 }
 
 func (DMChatObserver) Unsubscribe(key string) {
+	muDM.Lock()
+	defer muDM.Unlock()
+
 	close(dmChatObserver[key])
 	delete(dmChatObserver, key)
 }
 
 func (DMChatObserver) Send(key string, data any, event string) { // call in a new goroutine
+	muDM.Lock()
+	defer muDM.Unlock()
+
 	if mailbox, found := dmChatObserver[key]; found {
 		mailbox <- map[string]any{"event": event, "data": data}
 	} else {
@@ -47,25 +60,40 @@ func (DMChatObserver) Send(key string, data any, event string) { // call in a ne
 }
 
 func (DMChatObserver) SendPresenceUpdate(key string, data any, event string) {
+	muDM.Lock()
+	defer muDM.Unlock()
+
 	if mailbox, found := dmChatObserver[key]; found {
 		mailbox <- map[string]any{"event": event, "data": data}
 	}
 }
 
-var groupChatObserver = make(map[string]chan<- map[string]any)
+var (
+	muGroup           = sync.Mutex{}
+	groupChatObserver = make(map[string]chan<- map[string]any)
+)
 
 type GroupChatObserver struct{}
 
 func (GroupChatObserver) Subscribe(key string, mailbox chan<- map[string]any) {
+	muGroup.Lock()
+	defer muGroup.Unlock()
+
 	groupChatObserver[key] = mailbox
 }
 
 func (GroupChatObserver) Unsubscribe(key string) {
+	muGroup.Lock()
+	defer muGroup.Unlock()
+
 	close(groupChatObserver[key])
 	delete(groupChatObserver, key)
 }
 
 func (GroupChatObserver) Send(key string, data any, event string) { // call in a new goroutine
+	muGroup.Lock()
+	defer muGroup.Unlock()
+
 	if mailbox, found := groupChatObserver[key]; found {
 		mailbox <- map[string]any{"event": event, "data": data}
 	} else {
@@ -80,21 +108,33 @@ func (GroupChatObserver) Send(key string, data any, event string) { // call in a
 	}
 }
 
-var dMChatSessionObserver = make(map[string]chan<- map[string]any)
+var (
+	muDMSess              = sync.Mutex{}
+	dmChatSessionObserver = make(map[string]chan<- map[string]any)
+)
 
 type DMChatSessionObserver struct{}
 
 func (DMChatSessionObserver) Subscribe(key string, mailbox chan<- map[string]any) {
-	dMChatSessionObserver[key] = mailbox
+	muDMSess.Lock()
+	defer muDMSess.Unlock()
+
+	dmChatSessionObserver[key] = mailbox
 }
 
 func (DMChatSessionObserver) Unsubscribe(key string) {
-	close(dMChatSessionObserver[key])
-	delete(dMChatSessionObserver, key)
+	muDMSess.Lock()
+	defer muDMSess.Unlock()
+
+	close(dmChatSessionObserver[key])
+	delete(dmChatSessionObserver, key)
 }
 
 func (DMChatSessionObserver) Send(key string, data any, event string) { // call in a new goroutine
-	if mailbox, found := dMChatSessionObserver[key]; found {
+	muDMSess.Lock()
+	defer muDMSess.Unlock()
+
+	if mailbox, found := dmChatSessionObserver[key]; found {
 		mailbox <- map[string]any{"event": event, "data": data}
 	} else {
 		var (
@@ -112,20 +152,32 @@ func (DMChatSessionObserver) Send(key string, data any, event string) { // call 
 	}
 }
 
-var groupChatSessionObserver = make(map[string]chan<- map[string]any)
+var (
+	muGroupSess              = sync.Mutex{}
+	groupChatSessionObserver = make(map[string]chan<- map[string]any)
+)
 
 type GroupChatSessionObserver struct{}
 
 func (GroupChatSessionObserver) Subscribe(key string, mailbox chan<- map[string]any) {
+	muGroupSess.Lock()
+	defer muGroupSess.Unlock()
+
 	groupChatSessionObserver[key] = mailbox
 }
 
 func (GroupChatSessionObserver) Unsubscribe(key string) {
+	muGroupSess.Lock()
+	defer muGroupSess.Unlock()
+
 	close(groupChatSessionObserver[key])
 	delete(groupChatSessionObserver, key)
 }
 
 func (GroupChatSessionObserver) Send(key string, data any, event string) { // call in a new goroutine
+	muGroupSess.Lock()
+	defer muGroupSess.Unlock()
+
 	if mailbox, found := groupChatSessionObserver[key]; found {
 		mailbox <- map[string]any{"event": event, "data": data}
 	} else {
