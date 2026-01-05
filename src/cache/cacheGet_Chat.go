92b6f3e934e71d4166cb3c3e26756b88b900a43d
the-real-i9 <oluwarinolasam@gmail.com>
2026-01-05 19:14:52 +0100

    generate getURLs on request

--- src/cache/cacheGet_Chat.go
+++ src/cache/cacheGet_Chat.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"fmt"
 	"i9chat/src/helpers"
+	"slices"
 
 	"github.com/redis/go-redis/v9"
 )
 
-// Do here
 func GetGroup[T any](ctx context.Context, groupId string) (group T, err error) {
 	groupJson, err := rdb().HGet(ctx, "groups", groupId).Result()
 	if err != nil && err != redis.Nil {
@@ -16,7 +16,41 @@ func GetGroup[T any](ctx context.Context, groupId string) (group T, err error) {
 		return group, err
 	}
 
-	return helpers.FromJson[T](groupJson), nil
+	groupMap := helpers.FromJson[map[string]any](groupJson)
+
+	picCloudName := groupMap["picture_cloud_name"].(string)
+
+	var (
+		smallPicn  string
+		mediumPicn string
+		largePicn  string
+	)
+
+	_, err = fmt.Sscanf(picCloudName, "small:%s medium:%s large:%s", &smallPicn, &mediumPicn, &largePicn)
+	if err != nil {
+		return group, err
+	}
+
+	smallPicUrl, err := getMediaurl(smallPicn)
+	if err != nil {
+		return group, err
+	}
+
+	mediumPicUrl, err := getMediaurl(mediumPicn)
+	if err != nil {
+		return group, err
+	}
+
+	largePicUrl, err := getMediaurl(largePicn)
+	if err != nil {
+		return group, err
+	}
+
+	groupMap["picture_url"] = fmt.Sprintf("small:%s medium:%s large:%s", smallPicUrl, mediumPicUrl, largePicUrl)
+
+	delete(groupMap, "picture_cloud_name")
+
+	return helpers.ToStruct[T](groupMap), nil
 }
 
 func GetGroupMembersList(ctx context.Context, groupId string) ([]string, error) {
@@ -69,7 +103,6 @@ func GetChatUnreadMsgsCount(ctx context.Context, ownerUser, chatIdent string) (i
 	return count, nil
 }
 
-// Do here
 func GetDirectChatHistoryEntry[T any](ctx context.Context, CHEId string) (CHE T, err error) {
 	CHEJson, err := rdb().HGet(ctx, "direct_chat_history_entries", CHEId).Result()
 	if err != nil && err != redis.Nil {
@@ -77,10 +110,62 @@ func GetDirectChatHistoryEntry[T any](ctx context.Context, CHEId string) (CHE T,
 		return CHE, err
 	}
 
-	return helpers.FromJson[T](CHEJson), nil
+	CHEMap := helpers.FromJson[map[string]any](CHEJson)
+
+	cheType := CHEMap["che_type"].(string)
+
+	if cheType == "message" {
+		content := CHEMap["content"].(map[string]any)
+		contentProps := content["props"].(map[string]any)
+
+		if content["type"].(string) != "text" {
+			mediaCloudName := contentProps["media_cloud_name"].(string)
+
+			if slices.Contains([]string{"photo", "video"}, content["type"].(string)) {
+				var (
+					blurPlchMcn string
+					actualMcn   string
+				)
+
+				_, err = fmt.Sscanf(mediaCloudName, "blur_placeholder:%s actual:%s", &blurPlchMcn, &actualMcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				blurPlchUrl, err := getMediaurl(blurPlchMcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				actualUrl, err := getMediaurl(actualMcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				contentProps["media_url"] = fmt.Sprintf("blur_placeholder:%s actual:%s", blurPlchUrl, actualUrl)
+			} else {
+				var mcn string
+
+				_, err = fmt.Sscanf(mediaCloudName, "%s", &mcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				mediaUrl, err := getMediaurl(mcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				contentProps["media_url"] = mediaUrl
+			}
+
+			delete(contentProps, "media_cloud_name")
+		}
+	}
+
+	return helpers.ToStruct[T](CHEMap), nil
 }
 
-// Do here
 func GetGroupChatHistoryEntry[T any](ctx context.Context, CHEId string) (CHE T, err error) {
 	CHEJson, err := rdb().HGet(ctx, "group_chat_history_entries", CHEId).Result()
 	if err != nil && err != redis.Nil {
@@ -88,7 +173,60 @@ func GetGroupChatHistoryEntry[T any](ctx context.Context, CHEId string) (CHE T, 
 		return CHE, err
 	}
 
-	return helpers.FromJson[T](CHEJson), nil
+	CHEMap := helpers.FromJson[map[string]any](CHEJson)
+
+	cheType := CHEMap["che_type"].(string)
+
+	if cheType == "message" {
+		content := CHEMap["content"].(map[string]any)
+		contentProps := content["props"].(map[string]any)
+
+		if content["type"].(string) != "text" {
+			mediaCloudName := contentProps["media_cloud_name"].(string)
+
+			if slices.Contains([]string{"photo", "video"}, content["type"].(string)) {
+				var (
+					blurPlchMcn string
+					actualMcn   string
+				)
+
+				_, err = fmt.Sscanf(mediaCloudName, "blur_placeholder:%s actual:%s", &blurPlchMcn, &actualMcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				blurPlchUrl, err := getMediaurl(blurPlchMcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				actualUrl, err := getMediaurl(actualMcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				contentProps["media_url"] = fmt.Sprintf("blur_placeholder:%s actual:%s", blurPlchUrl, actualUrl)
+			} else {
+				var mcn string
+
+				_, err = fmt.Sscanf(mediaCloudName, "%s", &mcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				mediaUrl, err := getMediaurl(mcn)
+				if err != nil {
+					return CHE, err
+				}
+
+				contentProps["media_url"] = mediaUrl
+			}
+
+			delete(contentProps, "media_cloud_name")
+		}
+	}
+
+	return helpers.ToStruct[T](CHEMap), nil
 }
 
 func GetMsgReactions(ctx context.Context, msgId string) (map[string]string, error) {
--- src/cache/cacheGet_User.go
+++ src/cache/cacheGet_User.go
@@ -2,12 +2,12 @@ package cache
 
 import (
 	"context"
+	"fmt"
 	"i9chat/src/helpers"
 
 	"github.com/redis/go-redis/v9"
 )
 
-// Do here
 func GetUser[T any](ctx context.Context, username string) (user T, err error) {
 	userJson, err := rdb().HGet(ctx, "users", username).Result()
 	if err != nil && err != redis.Nil {
@@ -15,5 +15,39 @@ func GetUser[T any](ctx context.Context, username string) (user T, err error) {
 		return user, err
 	}
 
-	return helpers.FromJson[T](userJson), nil
+	userMap := helpers.FromJson[map[string]any](userJson)
+
+	ppicCloudName := userMap["profile_pic_cloud_name"].(string)
+
+	var (
+		smallPPicn  string
+		mediumPPicn string
+		largePPicn  string
+	)
+
+	_, err = fmt.Sscanf(ppicCloudName, "small:%s medium:%s large:%s", &smallPPicn, &mediumPPicn, &largePPicn)
+	if err != nil {
+		return user, err
+	}
+
+	smallPicUrl, err := getMediaurl(smallPPicn)
+	if err != nil {
+		return user, err
+	}
+
+	mediumPicUrl, err := getMediaurl(mediumPPicn)
+	if err != nil {
+		return user, err
+	}
+
+	largePicUrl, err := getMediaurl(largePPicn)
+	if err != nil {
+		return user, err
+	}
+
+	userMap["profile_pic_url"] = fmt.Sprintf("small:%s medium:%s large:%s", smallPicUrl, mediumPicUrl, largePicUrl)
+
+	delete(userMap, "profile_pic_cloud_name")
+
+	return helpers.ToStruct[T](userMap), nil
 }
